95fd334b7f2c78fb48ee331d4c797362c4f29566
Chepizhna Alina bc2024-5 <alina.chepizhna@lnu.edu.ua>
2025-05-20 14:39:15 +0300

    Рефакторинг структури проєкту додано images

--- theater-booking/src/data/performances.js
+++ theater-booking/src/data/performances.js
@@ -2,16 +2,82 @@ const performances = [
   {
     id: 1,
     title: 'Гамлет',
-    genre: 'Драма',
+    genre: 'Трагедія',
     time: '19:00',
-    description: 'Класична трагедія Шекспіра.'
+    description: 'Іконічна трагедія Вільяма Шекспіра про принца Данії.',
+    image: '/images/hamlet.jpg'
   },
   {
     id: 2,
-    title: 'Ревізор',
+    title: 'Знедолені',
+    genre: 'Мюзикл',
+    time: '18:30',
+    description: 'Епічна музична адаптація роману Віктора Гюго.',
+    image: '/images/les-miserables.jpg'
+  },
+  {
+    id: 3,
+    title: 'Привид опери',
+    genre: 'Мюзикл',
+    time: '20:00',
+    description: 'Містична історія кохання в Паризькій опері.',
+    image: '/images/phantom.jpg'
+  },
+  {
+    id: 4,
+    title: 'Смерть комівояжера',
+    genre: 'Драма',
+    time: '19:30',
+    description: 'Трагічна історія американської мрії.',
+    image: '/images/salesman.jpg'
+  },
+  {
+    id: 5,
+    title: 'Трамвай "Бажання"',
+    genre: 'Драма',
+    time: '19:00',
+    description: 'Психологічна драма про ілюзії, втрати та пристрасть.',
+    image: '/images/streetcar.jpg'
+  },
+  {
+    id: 6,
+    title: 'Як важливо бути серйозним',
     genre: 'Комедія',
     time: '18:00',
-    description: 'Гоголь оживає на сцені.'
+    description: 'Дотепна сатира Оскара Вайлда.',
+    image: '/images/earnest.jpg'
+  },
+  {
+    id: 7,
+    title: 'Король Лір',
+    genre: 'Трагедія',
+    time: '20:00',
+    description: 'Трагедія про владу, зраду і батьківську любов.',
+    image: '/images/lear.jpg'
+  },
+  {
+    id: 8,
+    title: 'Чекаючи на Годо',
+    genre: 'Театр абсурду',
+    time: '17:30',
+    description: 'Філософська п’єса про сенс життя.',
+    image: '/images/godot.jpg'
+  },
+  {
+    id: 9,
+    title: 'Отелло',
+    genre: 'Трагедія',
+    time: '19:45',
+    description: 'Трагедія про ревнощі та маніпуляції.',
+    image: '/images/othello.jpg'
+  },
+  {
+    id: 10,
+    title: 'Чайка',
+    genre: 'Драма',
+    time: '18:15',
+    description: 'П’єса про мистецтво та розчарування.',
+    image: '/images/seagull.jpg'
   }
 ];
 
